72294be662c4e78333d718eb59e9902f66c85163
lutiesheng <ltszjhz@163.com>
2016-12-01 22:14:43 +0800

    Initial Commit

---
+++ TouchPainer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TouchPainer
+//
+//  Created by 陆铁生 on 2016/12/1.
+//  Copyright © 2016年 陆铁生. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TouchPainer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TouchPainer
+//
+//  Created by 陆铁生 on 2016/12/1.
+//  Copyright © 2016年 陆铁生. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
